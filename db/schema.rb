6fcab90227f38eebf55868784b215bdc517c209a
BuildTools <unconfigured@null.spigotmc.org>
2018-03-26 16:31:02 -0400

    Added Image post support
    Posts require a kind field

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325210504) do
+ActiveRecord::Schema.define(version: 20180326200341) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "kind"
   end
 
 end
